46f22d48eba5322a52f7874bf1a08f60d75f6f3c
vinod-pyapali <110025744+vinod-pyapali@users.noreply.github.com>
2022-07-26 15:41:33 +0530

    Update streamlit_app.py
    new item with emojis added

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,4 @@ streamlit.text('🥣 Omega 3 and Blueberry Oatmeal');
 streamlit.text('🥗 Kale, Spinach and Rocket Smoothie');               
 streamlit.text('🐔 Hard boiled free-range egg');  
 streamlit.text('🥑 🍞 Avacado Toast');  
-
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
